d12b90e172169c9bf843a88534c549390be039f8
GustavoXlh3 <gustavoxlh3@gmail.com>
2022-06-01 20:03:29 -0300

    criando personagem e movendo ele

--- sketch.js
+++ sketch.js
@@ -1,11 +1,25 @@
+var potatoSaver;
+var potatoSaverImg, panImg, badPanImg, potatoImg;
+
+function preload(){
+  potatoSaverImg = loadImage("./assets/potatoSaver.gif");
+}
 
 function setup() {
-  
-  createCanvas(400, 400);
+  createCanvas(800, 600);
+  potatoSaver = createSprite(300, 60);
+  potatoSaver.addImage(potatoSaverImg);
+  potatoSaver.scale = 0.1;
 }
 
 function draw() {
   // limpa a tela
   background('black');
-
+  if (keyDown("left")){
+    potatoSaver.x -= 5;
+  }
+  if (keyDown("right")){
+    potatoSaver.x += 5;
+  }
+  drawSprites();
 }
