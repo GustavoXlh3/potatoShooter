b4e02411bc289d5f1c33b9bacd70ff10f5dfd530
GustavoXlh3 <gustavoxlh3@gmail.com>
2022-06-02 17:46:49 -0300

    mudando o personagem e adicionando animaçao dele

--- sketch.js
+++ sketch.js
@@ -1,15 +1,18 @@
 var potatoSaver;
-var potatoSaverImg, panImg, badPanImg, potatoImg;
+var potatoSaverImg, potatoSaverIdleImg, panImg, badPanImg, potatoImg;
 
 function preload(){
   potatoSaverImg = loadImage("./assets/potatoSaver.gif");
+  potatoSaverIdleImg = loadImage("./assets/potatoSaverIdle.gif");
+  panImg = loadImage("./assets/pan.png");
+  badPanImg = loadImage("./assets/bad pan.png");
+  potatoImg = loadImage("./assets/potato.png");
 }
 
 function setup() {
   createCanvas(800, 600);
   potatoSaver = createSprite(300, 60);
-  potatoSaver.addImage(potatoSaverImg);
-  potatoSaver.scale = 0.1;
+  potatoSaver.addImage(potatoSaverIdleImg);
 }
 
 function draw() {
@@ -17,9 +20,11 @@ function draw() {
   background('black');
   if (keyDown("left")){
     potatoSaver.x -= 5;
+    potatoSaver.changeImage(potatoSaverImg);
   }
   if (keyDown("right")){
     potatoSaver.x += 5;
+    potatoSaver.changeImage(potatoSaverImg);
   }
   drawSprites();
 }
