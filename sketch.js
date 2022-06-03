f74fa5e1435e73396a340db7ad39d563f072b63f
GustavoXlh3 <gustavoxlh3@gmail.com>
2022-06-03 20:00:26 -0300

    adicionando as panelas e mudando a animaçao do pla

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
 var potatoSaver;
 var potatoSaverImg, potatoSaverIdleImg, panImg, badPanImg, potatoImg;
+var panGroup;
 
 function preload(){
-  potatoSaverImg = loadImage("./assets/potatoSaver.gif");
-  potatoSaverIdleImg = loadImage("./assets/potatoSaverIdle.gif");
+  potatoSaverImg = loadAnimation("./assets/potatoSaver/sprite_0.png", "./assets/potatoSaver/sprite_1.png", "./assets/potatoSaver/sprite_2.png", "./assets/potatoSaver/sprite_3.png");
+  potatoSaverIdleImg = loadAnimation("./assets/potatoSaverIdle/sprite_0.png", "./assets/potatoSaverIdle/sprite_1.png", "./assets/potatoSaverIdle/sprite_2.png", "./assets/potatoSaverIdle/sprite_3.png", "./assets/potatoSaverIdle/sprite_4.png");
   panImg = loadImage("./assets/pan.png");
   badPanImg = loadImage("./assets/bad pan.png");
   potatoImg = loadImage("./assets/potato.png");
@@ -12,7 +13,11 @@ function preload(){
 function setup() {
   createCanvas(800, 600);
   potatoSaver = createSprite(300, 60);
-  potatoSaver.addImage(potatoSaverIdleImg);
+  potatoSaver.addAnimation("jogador parado", potatoSaverIdleImg);
+  potatoSaver.addAnimation("jogador andando", potatoSaverImg);
+  potatoSaver.scale = 1.8;
+
+  panGroup = new Group(); 
 }
 
 function draw() {
@@ -20,11 +25,27 @@ function draw() {
   background('black');
   if (keyDown("left")){
     potatoSaver.x -= 5;
-    potatoSaver.changeImage(potatoSaverImg);
+    potatoSaver.changeAnimation("jogador andando");
   }
   if (keyDown("right")){
     potatoSaver.x += 5;
-    potatoSaver.changeImage(potatoSaverImg);
+    potatoSaver.changeAnimation("jogador andando");
   }
+  if (keyWentUp("right") || keyWentUp("left")){
+    potatoSaver.changeAnimation("jogador parado");
+  }
+  gerarPanelas(10);
+  
   drawSprites();
+}
+
+function gerarPanelas(x){
+if (frameCount % x == 0){
+ var panela = createSprite(random(0,800), 610, 10, 10);
+ panela.addImage("panela", panImg);
+ panela.scale = 2;
+ panela.velocityY = -5;
+ panela.lifetime = -width/panela.velocityY;
+ panGroup.add(panela);
+}
 }
