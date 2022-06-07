02225a404ec54681fdcacb414fbba70364301346
GustavoXlh3 <gustavoxlh3@gmail.com>
2022-06-07 19:23:27 -0300

    arrumando sprites adicionando vida e atirando batatas

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
 var potatoSaver;
-var potatoSaverImg, potatoSaverIdleImg, panImg, badPanImg, potatoImg;
-var panGroup;
+var potatoSaverImg, potatoSaverIdleImg, panImg, badPanImg, potatoImg, potatoHeartImg;
+var panGroup, potatoGroup;
+var life = 3;
+//menu, jogo1(), jogo2(), jogo3(), jogo4();
+var state = "menu";
+
 
 function preload(){
   potatoSaverImg = loadAnimation("./assets/potatoSaver/sprite_0.png", "./assets/potatoSaver/sprite_1.png", "./assets/potatoSaver/sprite_2.png", "./assets/potatoSaver/sprite_3.png");
@@ -8,44 +12,66 @@ function preload(){
   panImg = loadImage("./assets/pan.png");
   badPanImg = loadImage("./assets/bad pan.png");
   potatoImg = loadImage("./assets/potato.png");
+  potatoHeartImg = loadImage("./assets/potatoHeart.png");
 }
 
 function setup() {
   createCanvas(800, 600);
-  potatoSaver = createSprite(300, 60);
+  potatoSaver = createSprite(300, 70);
   potatoSaver.addAnimation("jogador parado", potatoSaverIdleImg);
   potatoSaver.addAnimation("jogador andando", potatoSaverImg);
-  potatoSaver.scale = 1.8;
 
   panGroup = new Group(); 
+  potatoGroup = new Group();
 }
 
 function draw() {
   // limpa a tela
   background('black');
   if (keyDown("left")){
-    potatoSaver.x -= 5;
+    potatoSaver.x -= 10;
     potatoSaver.changeAnimation("jogador andando");
   }
   if (keyDown("right")){
-    potatoSaver.x += 5;
+    potatoSaver.x += 10;
     potatoSaver.changeAnimation("jogador andando");
   }
   if (keyWentUp("right") || keyWentUp("left")){
     potatoSaver.changeAnimation("jogador parado");
   }
-  gerarPanelas(10);
-  
+  gerarPanelas(100);
+  dispararBatatas();
+  mostrarVida(life);
+
   drawSprites();
 }
 
 function gerarPanelas(x){
-if (frameCount % x == 0){
- var panela = createSprite(random(0,800), 610, 10, 10);
- panela.addImage("panela", panImg);
- panela.scale = 2;
- panela.velocityY = -5;
- panela.lifetime = -width/panela.velocityY;
- panGroup.add(panela);
+  if (frameCount % x == 0){
+   var panela = createSprite(random(0,800), 610, 10, 10);
+   panela.addImage("panela", panImg);
+   panela.velocityY = -5;
+   panela.lifetime = -width/panela.velocityY;
+   panGroup.add(panela);
+  }
 }
+
+function transformarPanelas(){
+
+}
+
+function dispararBatatas(){
+  if (keyDown("space") && (potatoGroup.size() == 0 || potatoGroup.get(potatoGroup.size()-1).y>200)){
+    var potato = createSprite(potatoSaver.x, potatoSaver.y);
+    potato.addImage(potatoImg);
+    potato.velocityY = +8;
+    potato.lifetime = width/potato.velocityY;
+    potatoGroup.add(potato);
+  }
+}
+
+function mostrarVida(x){
+  for (var i = 1; i<= x; i++){
+    image(potatoHeartImg, width-30*i, 5, 20, 20);
+  }
 }
